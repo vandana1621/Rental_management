e85e8c2ff04ebb52270f17cefc7ab4cfb86f4276
Vandana Chauhan Ajara <31502395+vandana1621@users.noreply.github.com>
2024-05-31 23:05:01 +0530

    Update settings.py

--- wms2/settings.py
+++ wms2/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'wms2.urls'
@@ -119,12 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+# Add static files settings
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'product_tracking/static')]
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+# Enable Gzip and Cache
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Media settings
 
 MEDIA_URL = '/media/'
